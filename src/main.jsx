801c036445d373d19450fd915d1ca3315f995fee
danielakoch3011-cloud <danielakoch3011@gmail.com>
2026-05-09 08:13:10 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GolfTrackUnicorn from "./App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GolfTrackUnicorn />
+    <App />
   </React.StrictMode>
 );

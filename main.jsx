3b48e156c174e2e525310f95ea785a80690f64c0
danielakoch3011-cloud <danielakoch3011@gmail.com>
2026-05-09 04:50:59 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GolfTrackUnicorn from "./golf_track_unicorn.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GolfTrackUnicorn />
+  </React.StrictMode>
+);

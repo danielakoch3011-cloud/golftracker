bf6c664f542b3a83c154849292dc588c41d681b2
danielakoch3011-cloud <danielakoch3011@gmail.com>
2026-05-12 03:24:21 +0200

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { cn } from "../utils";
 
+export default function Card({ children, className = "" }) {
+  return (
+    <section
+      className={cn(
+        "rounded-[1.35rem] border border-slate-200 bg-white p-6 shadow-[0_18px_45px_rgba(15,23,42,0.04)]",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+}
